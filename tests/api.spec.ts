d303434d0ec1b2395960af0dda5094f8493a0ec0
kirilaf78 <kirilaf78@gmail.com>
2025-11-16 13:30:33 -0500

    adding test_data file and refactoring the code

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,24 +1,18 @@
 import { test, expect } from '@playwright/test';
+import { validCredentials, failedCredentials } from '../test_data';
 
 test.describe('API tests', () => {
-  test.use({
-    extraHTTPHeaders: {
-      'x-api-key': 'reqres-free-v1',
-    },
-  });
+
   test('GET /api/login', async ({ request }) => {
-    const response = await request.get('https://reqres.in/api/login');
+    const response = await request.get('/api/login');
     expect(response.status()).toBe(200);
     const json = await response.json();
     expect(json.data).toHaveLength(6);
   });
 
   test('POST /api/register - success', async ({ request }) => {
-    const response = await request.post('https://reqres.in/api/register', {
-      data: {
-        email: 'eve.holt@reqres.in',
-        password: 'anypassword',
-      },
+    const response = await request.post('/api/register', {
+      data: validCredentials.credentials
     });
     expect(response.status()).toBe(200);
     const json = await response.json();
@@ -26,29 +20,18 @@ test.describe('API tests', () => {
     expect(typeof json.token).toBe('string');
   });
 
-  test('POST /api/register - failure', async ({ request }) => {
-    const testCases = [
-      {
-        payload: { email: 'eve.holt@reqres.in' },
-        errorMessage: 'Missing password',
-      },
-      {
-        payload: { password: 'anypassword' },
-        errorMessage: 'Missing email or username',
-      },
-      {
-        payload: { email: 'undefined@email.com', password: 'anypassword' },
-        errorMessage: 'Note: Only defined users succeed registration',
-      },
-    ];
 
-    for (const testCase of testCases) {
-      const response = await request.post('https://reqres.in/api/register', {
-        data: testCase.payload,
-      });
-      expect(response.status()).toBe(400);
-      const json = await response.json();
-      expect(json.error).toBe(testCase.errorMessage);
-    }
+for (const testCase of failedCredentials) {
+  
+  test(`POST /api/register - failure (${testCase.name})`, async ({ request }) => {
+    const response = await request.post('/api/register', {
+      data: testCase.payload,
+    });
+
+    expect(response.status()).toBe(400);
+    const json = await response.json();
+    expect(json.error).toBe(testCase.errorMessage);
   });
+}
+
 });
